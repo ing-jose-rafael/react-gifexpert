480e6e961181bef1e087ac8810337fc8a4972f3e
josecarreno <ing.jcarreno@gmail.com>
2022-10-30 21:57:26 -0500

    useLayoutEffect

--- src/03-examples/Quote.jsx
+++ src/03-examples/Quote.jsx
@@ -1,9 +1,22 @@
+import { useLayoutEffect, useRef, useState } from "react"
 
-export const Quote = ({quote, author}) => {
+export const Quote = ({ quote, author }) => {
+  const pRef = useRef();
+  const [sizeBox, setSizeBox] = useState({ width: 0, height: 0 })
+  // se dispara despues que se renderiza el componente
+  useLayoutEffect(() => {
+    const {width, height} = pRef.current.getBoundingClientRect();
+    setSizeBox({ width, height });
+  }, [quote]);
+
   return (
-    <blockquote className="blockquote text-end">
-      <p className="md-1">{quote}</p>
-      <footer className="blockquote-footer">{author}</footer>
-    </blockquote>
+    <>
+      <blockquote className="blockquote text-end" style={{ display: 'flex' }}>
+        <p ref={pRef} className="md-1">{quote}</p>
+        <footer className="blockquote-footer">{author}</footer>
+      </blockquote>
+      <code>{JSON.stringify(sizeBox)}</code>
+
+    </>
   )
 }
---
+++ src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,42 @@
+import { useFetch, useCounter } from "../hooks"
+import { LoadingQuote, Quote } from "../03-examples";
+
+// import { useCounter } from "../hooks/useCounter"
+
+export const Layout = () => {
+  const {counter,increment,decrement } = useCounter(1)
+
+  const { data, hasError, isLoanding } = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+  // si la data tiene valor, y como llega como un arreglo toma la posicion 1
+  // si se tiene !null la repuesta es true, !!null es false, hacemos eso por que
+  // no se puede desectructurar un objeto vacio
+  const { quote, author } = !!data && data[0];
+
+  // if (isLoanding) {
+  //   return (
+  //     <div className="spinner-border text-primary" role="status">
+  //       <span className="visually-hidden">Cargando...</span>
+  //     </div>
+  //   )
+  // }
+  // aca no puede ir un Hooks, no pueden ser creado de manera condicional
+  return (
+    <>
+      <h1>BreakingBad Quotes</h1>
+      <hr />
+
+      {
+        isLoanding
+          ? <LoadingQuote/>
+          : <Quote quote={quote} author={author}/>
+      }
+
+      <button className="btn btn-primary" onClick={()=>decrement()} disabled = {isLoanding || counter<=1}>
+        Previous qoute
+      </button>
+      <button className="btn btn-primary" onClick={()=>increment()} disabled = {isLoanding}>
+        Next qoute
+      </button>
+    </>
+  )
+}
