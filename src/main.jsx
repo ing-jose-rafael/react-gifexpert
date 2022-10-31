41a478c4a188f0103d2d50a0fd2c5e58346018c3
josecarreno <ing.jcarreno@gmail.com>
2022-10-30 21:29:12 -0500

    useRef - Primer uso

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import ReactDOM from 'react-dom/client'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+import { FocusScreen } from './04-useRef/FocusScreen'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     // <CounterWithCustomHook/>
    // {/* <SimpleForm/> */}
     // <FormWithCustomHook/>
-    <MultipleCustomHooks/>
+    // <MultipleCustomHooks/>
+    <FocusScreen/>
   // </React.StrictMode>
 )
