7377f894ededf756d6852a43a63002ef4fc91f39
josecarreno <ing.jcarreno@gmail.com>
2022-11-19 18:21:52 -0500

    CreateContext y ContextProvider

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -3,12 +3,14 @@ import { HomePage } from './HomePage'
 import { LoginPage } from './LoginPage'
 import { AboutPage } from './AboutPage'
 import { Navbar } from './Navbar'
+import { UserProvider } from './context/UserProvider'
 export const MainApp = () => {
   return (
-    <>
+    <UserProvider>
       <h1>Main Page</h1>
       <Navbar/>
       <hr />
+
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
         <Route path='/login' element={<LoginPage />}></Route>
@@ -18,6 +20,6 @@ export const MainApp = () => {
         {/* <Route path='/*' element={<LoginPage />}></Route> */}
         <Route path='/*' element={<Navigate to='/login'/>}></Route>
       </Routes>
-    </>
+    </UserProvider>
   )
 }
---
+++ src/09-useContext/Navbar.jsx
@@ -0,0 +1,49 @@
+import { Link, NavLink } from "react-router-dom"
+
+export const Navbar = () => {
+  return (
+    <>
+      {/* <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/login'>Login</Link> */}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded-3">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">useConter</Link>
+          
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              {/* opcion 1 */}
+              {/* <li className="nav-item">
+                <Link className="nav-link" to="/about">About</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">Login</Link>
+              </li> */}
+
+              {/* opcion 2 */}
+              <NavLink 
+                className = {({isActive})=>`nav-link ${isActive ? 'active':''}`}
+                to = '/'
+              >
+                Home
+              </NavLink>
+              
+              <NavLink 
+                className = {({isActive})=>`nav-link ${isActive ? 'active':''}`}
+                to = '/about'
+              >About
+              </NavLink>
+
+              <NavLink 
+                className = {({isActive})=>`nav-link ${isActive ? 'active':''}`}
+                to = '/login'
+              >Login
+              </NavLink>
+             
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
