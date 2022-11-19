7c79aba807828cf25eea6c4d8ec5d53a021601dc
josecarreno <ing.jcarreno@gmail.com>
2022-11-19 17:58:30 -0500

    navlink

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -2,13 +2,12 @@ import { Routes, Route, Navigate, Link } from 'react-router-dom'
 import { HomePage } from './HomePage'
 import { LoginPage } from './LoginPage'
 import { AboutPage } from './AboutPage'
+import { Navbar } from './Navbar'
 export const MainApp = () => {
   return (
     <>
       <h1>Main Page</h1>
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
-      <Link to='/login'>Login</Link>
+      <Navbar/>
       <hr />
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
